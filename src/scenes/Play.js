b4e28a693b554b1d778988abc6c6811bbd93d6f7
Tiffany Thang <tthang@ucsc.edu>
2020-03-30 13:20:26 -0700

    third

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,16 @@ class Play extends Phaser.Scene{
         super("playScene");
     }
     
+    preload(){
+        //load images/tile sprites 
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
     create(){
+        //place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0,0);
         //white rectangle borders 
         this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0,0);
@@ -11,5 +20,9 @@ class Play extends Phaser.Scene{
         this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0,0);
         //green UI background
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0,0);
+    }
+
+    update(){
+        this.starfield.tilePositionX -= 4;
     }
 }
